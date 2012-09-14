e692daffb81936ec2478c4cea77eb031524bcead
Kajabla <kajabla@ubuntu.ubuntu-domain>
2012-09-13 22:21:25 -0400

    randomizer

--- jStevenson/randomizer.py
+++ jStevenson/randomizer.py
@@ -1,4 +1,11 @@
+import re, random
 def randomize(group_size, file):
-    lineList = open(file, "r").readlines().shuffle()
+    lineList = random.shuffle(open(file, "r").readlines())
     for line in lineList:
-        line = line.split( ,)
+        line = re.split(" |,", line)
+    print(line)
+    groupNum = list.length // group_size + 1
+    groupList = []
+    for i in range(groupNum):
+        groupList += lineList[4*i:4*(i+1)]
+    print(groupList)
