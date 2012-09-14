11d31ccfa91a0a4902c8ea23e5435d57cbeeede9
Brian <bzhang1670@gmail.com>
2012-09-14 01:13:21 -0400

    Just commiting files

---
+++ Bzhang/Program.py
@@ -0,0 +1,25 @@
+from random import randint
+
+def makeGroups(x):
+
+file = open("ml7-student-names", 'r')
+
+period6 = []
+period7 = []
+period6Groups = []
+period7Groups = []
+for line in file
+    if line[-1] = 6
+        period6.append(line)
+    else
+        period7.append(line)
+
+numberOfGroups6 = len(period6)/4
+numberOfGroups7 = len(period7)/4
+
+    #while numberOfGroups6 > 0:
+    
+    #x = 4
+    #period6groups 
+#while x > 0:
+      
