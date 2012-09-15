fbb97066a2a0258326108a034cd5cb40c1ab7950
Hayden Karp-Hecker <hayden.kh@gmail.com>
2012-09-14 21:54:37 -0400

    Finished HW

--- try.py
+++ try.py
@@ -1,11 +1,48 @@
+from random import shuffle
 
-for line in open("ml7-student-names","r").readlines():
+pd6 = []
+pd7 = []
+counter = 0
+counter2 = 0
+
+for line in open("ml7-student-names").readlines():
     line = line.strip()
     a,b,c = line.split(",")
-    print a
-    #print line
+    if c is '6':
+        pd6.append(b+a+c)
+    else:
+        pd7.append(b+a+c)
 
 
-import random
-x=random.random()
-x=x*
+#print pd6
+#shuffle(pd6)
+#print ""
+#print pd6
+
+#print ""
+#print pd7
+#shuffle(pd7)
+#print ""
+#print pd7
+
+counter = 0
+for name in pd6:
+    if counter <= 3:
+        print name
+        counter+= 1
+    else:
+        print 
+        print name
+        counter = 1
+
+print    
+
+counter = 0
+for name in pd7:
+    if counter <= 3:
+        print name
+        counter+= 1
+    else:
+        print
+        print name
+        counter = 1
