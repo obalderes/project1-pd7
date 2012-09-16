f982028125b9a0ab4a4ddeee1a8ff777e2cf6ed4
Batya Zamansky <batya@rufferto.(none)>
2012-09-15 20:57:17 -0400

    fixed error

--- mess-with-me.py
+++ mess-with-me.py
@@ -10,13 +10,13 @@ print "zachary added this awesome new stuff which is cool"
 print "batya added this stuff which is cool"
 
 zachary = ""
-for x in xrange(7,10)
+for x in xrange(7,10):
     zachary += "there are " + str(x) + " sheep and " + str(x-7) + " mutant sheep in a farm, until 1 mutant sheep is born, then "
 
 print zachary
 
 batya = ""
-for x in xrange(7,10)
+for x in xrange(7,10):
     batya += "there are " + str(x) + " sheep in new york, until 1 is born, then there are more"
 
 print batya
