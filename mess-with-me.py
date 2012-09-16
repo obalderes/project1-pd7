dec7847f343b216256cb3e2a9a7d964b86b1d778
Batya Zamansky <batya@rufferto.(none)>
2012-09-15 20:47:52 -0400

    g

--- mess-with-me.py
+++ mess-with-me.py
@@ -8,5 +8,5 @@ for i in range(5,8):
 
 print "zachary added this awesome new stuff which is cool"
 
-print "batya added this new stuff which is also cool"
+print "batya added this stuff which is cool"
 
