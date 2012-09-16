918f776b87f1a444b9e1e62b34fb684a05e742f6
Batya Zamansky <batya@rufferto.(none)>
2012-09-15 20:44:47 -0400

    d

--- mess-with-me.py
+++ mess-with-me.py
@@ -1,10 +1,12 @@
 print "hi people"
 
-<<<<<<< HEAD
+
 print "hi there"
 
-for i in range(5,10):
+for i in range(5,8):
     print i
-=======
+
 print "zachary added this awesome new stuff which is cool"
->>>>>>> 70cb36149ba115645d248c6a922cf78e5b6d444b
+
+print "batya added this new stuff which is also cool"
+
