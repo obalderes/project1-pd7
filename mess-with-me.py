dd0e5c7fa53ba6ee706a2d16ef6a4c18513b2669
Zachary Zee <Oneman2feet@gmail.com>
2012-09-15 20:43:08 -0400

    hi there stranger

--- mess-with-me.py
+++ mess-with-me.py
@@ -1,3 +1,9 @@
 print "hi people"
 
 print "zachary added this awesome new stuff which is cool"
+
+s = ""
+for x in xrange(7,10)
+    s += "there are " + str(x) + " sheep in a farm, until 1 is born, then "
+
+print s
