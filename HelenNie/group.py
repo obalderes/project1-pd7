a95912cccd3d75fc97a32cb3e0b6621f80a4bc14
HelenNie <artigkaninhn@gmail.com>
2012-09-16 01:30:38 -0400

    finished group.py

--- HelenNie/group.py
+++ HelenNie/group.py
@@ -3,52 +3,32 @@
 from random import randrange
 groupSize = 4
 
-
-#puts lines from ml7-student-names file in 'lines'
-f = open("names", "r")
-lines = f.readlines()
-
-#puts names in either 'pd6' or 'pd7'
+#creates a list of names for each period
 pd6 = []
 pd7 = []
-for line in lines:
-    #sets pd to the only digit in the line, either 6 or 7
-    pd = filter(str.isdigit, line)
-    if pd == "6":
-        #transforms line to firstName_space_lastName and adds to pd6
-        strippedLine = line.strip()
-        splitLine = strippedLine.split(',')
-        fullName = splitLine[1] + " " + splitLine[0]
+for line in open("names", "r").readlines():
+    #formats line to firstName_space_lastName
+    strippedLine = line.strip()
+    splitLine = strippedLine.split(',')
+    fullName = splitLine[1] + " " + splitLine[0]
+    #separates names into period 6 and 7
+    if splitLine[2] == "6":
         pd6.append(fullName)
     else:
-        #transforms line to firstName_space_lastName and adds to pd7
-        strippedLine = line.strip()
-        splitLine = strippedLine.split(',')
-        fullName = splitLine[1] + " " + splitLine[0]
         pd7.append(fullName)
 
-print "\n"
 
-#generates and prints random groups for period 6
-print "PD 6 GROUPS:"
-for i in range (1, len(pd6) / groupSize + 1):
-    print "\tGroup " + str(i) + ":"
-    #while pd6 isn't empty, lists out the members of current group
-    for i in range (0, groupSize):
-        if not pd6:
-            break
-        else:
-            print "\t\t" + str(pd6.pop(randrange(0, len(pd6))))
+#generates random groups for each period
+pds = [pd6, pd7]
+pdHeadings = ["PD 6 GROUPS", "PD 7 GROUPS"]
 
-print "\n"
-
-#generates and prints random groups for period 7
-print "PD 7 GROUPS:"
-for i in range (1, len(pd7) / groupSize + 1):
-    print "\tGroup " + str(i) + ":"
-    #while pd7 isn't empty, lists out the members of current group   
-    for i in range (0, groupSize):
-        if not pd7:
-            break
-        else:
-            print "\t\t" + str(pd7.pop(randrange(0, len(pd7))))
+for pd in pds:
+    print "\n" + pdHeadings.pop(0)
+    for i in range (1, len(pd) / groupSize + 1):
+        print "\tGroup " + str(i) + ":"
+        #while period isn't empty, prints members of current group
+        for i in range (0, groupSize):
+            if not pd:
+                break
+            else:
+                print "\t\t" + str(pd.pop(randrange(0, len(pd))))
