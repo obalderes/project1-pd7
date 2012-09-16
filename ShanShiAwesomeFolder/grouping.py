3ff243fdad52146239a9c802c9713b143b981fbc
Shan Shi <a455898334@gmail.com>
2012-09-15 17:07:31 -0700

    printing out results instead of outputting to file

--- ShanShiAwesomeFolder/grouping.py
+++ ShanShiAwesomeFolder/grouping.py
@@ -1,9 +1,9 @@
 from random import shuffle
 
-#import ml7-student-names line-by-line into lines
+##import ml7-student-names line-by-line into lines
 lines = [line.strip() for line in open('ml7-student-names')]
 
-#split lines into two separate periods and remove the period numbers
+##split lines into two separate periods and remove the period numbers
 pd6students = []
 for pd6student in lines:
     if pd6student[-1] == '6':
@@ -13,11 +13,11 @@ for pd7student in lines:
     if pd7student[-1] == '7':
         pd7students.append(pd7student[0:-2])
 
-#shuffle the periods
+##shuffle the periods
 shuffle(pd6students)
 shuffle(pd7students)
 
-#split the periods into groups and put everything in a string
+##split the periods into groups and put everything in a string
 counter = 0 
 
 out = "Period 6\n"
@@ -38,7 +38,10 @@ while len(pd7students) > 0:
         out += "\n"
         counter = 1
 
-#output the string into a tsv file
-text_file = open("groups.csv", "w")
-text_file.write(out)
-text_file.close()
+##output the string into a tsv file
+#text_file = open("groups.csv", "w")
+#text_file.write(out)
+#text_file.close()
+
+#print out the string
+print out
