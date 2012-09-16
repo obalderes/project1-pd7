af860cfdae868815149314dc327a81ec8b39a76f
David Kurkovskiy <brainmonkey95@gmail.com>
2012-09-16 13:26:42 -0400

    Made some changes.

--- fileToTest.py
+++ fileToTest.py
@@ -1,15 +1,18 @@
 #this is the file we can edit at the same time
 
-print "Hello World"
+print "Hello World.", "What's up?"
 
 #i feel like there should be some real code in here so here it is sort of
 
 a = []
 
 for i in range(5,15):
-    if (i*i)%2 == 0:
+    if (i*i)%2 == 1:
         a.append(i)
+        print "Odd number alert!"
 
 print a
 
 #and that was some code
+
+#Great code! :) 
