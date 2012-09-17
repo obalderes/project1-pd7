12c63af652223904b529937f27f4716aa9bf324c
Zambeta Tsapos <ztsapos@gmail.com>
2012-09-17 16:56:45 -0400

    Second try

--- Zambeta_Tsapos/first_project.py
+++ Zambeta_Tsapos/first_project.py
@@ -1,34 +1,33 @@
-import random.py
-names = read("ml7-student-names");
+import random
 pdSix = list()
 pdSeven = list()
 pd7Groups = list()
 pd6Groups = list()
-for line in open(names, "r").readlines():
+for line in open("ml7-student-names", "r").readlines():
     name = line.strip().split(",")
-    if name[2] = 7:
+    if name[2] == 7:
        pdSeven.append(name)
-    else
+    else:
         pdSix.append(name)
 counter7 = len(pdSeven)
 counter6 = len(pdSix)
 groups7 = counter7 / 4
 groups6 = counter6 / 4
-while counter7 > 0
+while groups7 > 0:
     pd7Groups.append(list())
-    counter7 = counter7 - 1
-while counter6 > 0
+    groups7 = groups7 - 1
+while groups6 > 0:
     pd6Groups.append(list())
-    counter6 = counter6 - 1
+    groups6 = groups6 - 1
 for aName in pdSeven:
-    number = random.randrange(0, groups7 - 1, 1)
-    while (len(pd7Groups[number] > 4):
-           number = random.randrange(0, groups7 - 1, 1)
+    number = randrange(0, groups7 - 1, 1)
+    while len(pd7Groups[number]) > 4:
+           number = randrange(0, groups7 - 1, 1)
     pd7Groups[number].append(aName)
 for aName in pdSix:
-    number = random.randrange(0, groups6 - 1, 1)
-    while (len(pd6Groups[number] > 4):
-           number = random.randrange(0, groups7 - 1, 1)
+    number = randrange(0, groups6 - 1, 1)
+    while len(pd6Groups[number]) > 4:
+           number = randrange(0, groups7 - 1, 1)
     pd7Groups[number].append(aName)
 print pd7Groups
 print pd6Groups
