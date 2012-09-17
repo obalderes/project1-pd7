454da79b37507600eeffc0082d36413c8c6233b9
Dina Levy-Lambert <dina@Dina-Levy-Lamberts-iMac.local>
2012-09-17 11:54:06 -0400

    testing splitting into the periods

--- Dina_LevyLambert/test.py
+++ Dina_LevyLambert/test.py
@@ -1,1 +1,26 @@
-#EMPTY TEST FILE
+#!/user/bin/python
+
+import random
+groupNumber = 4
+
+#period lists
+p6 = []
+p7 = []
+
+#reformat line and divide the list of names into their periods
+
+for line in open("ml7-student-names", "r").readlines():
+    newLine = line.strip()
+    goodLine = newLine.split(',')
+    betterLine = goodLine[1] + " " + goodLine[0]
+    if goodLine[2] == "7":
+        p7.append(betterLine)
+    else:
+        p6.append(betterLine)
+
+print p7, p6
+
+
+
+
+                            
