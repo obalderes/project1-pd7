2faa250ababa6d43e630fc8a297aa04bfbbecec7
Brian Yan <sorakeybade@gmail.com>
2012-09-17 00:22:51 -0400

    My solution

--- Yan_Brian/Sort.py
+++ Yan_Brian/Sort.py
@@ -1,3 +1,4 @@
+import random
 period6 = list()
 period7 = list()
 
@@ -11,6 +12,25 @@ for text in open("ml7-student-names","r").readlines():
         period6.append(firstname + " " + lastname)
     else:
         period7.append(firstname + " " + lastname)
-print period6[80]
-print period6
-print period7
+
+random.shuffle(period6)
+random.shuffle(period7)
+
+group6 = ""
+group7 = ""
+index6 = 0
+index7 = 0
+print "Period 6"
+while len(period6) > index6:
+    if index6 % 4 == 0:
+        group6 = group6 + "\n"
+    group6 = group6+"\n"+period6[index6]
+    index6 = index6 + 1
+print "Period 7"
+while len(period7) > index7:
+    if index7 % 4 == 0:
+        group7 = group7 + "\n"
+    group7 = group7+"\n"+period7[index7]
+    index7 = index7 + 1
+print group6
+print group7
