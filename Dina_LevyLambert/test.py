8a9d3f43ad79c404e28530cca3c60cc66f03cb94
Dina Levy-Lambert <dina@Dina-Levy-Lamberts-iMac.local>
2012-09-17 16:26:50 -0400

    last change

--- Dina_LevyLambert/test.py
+++ Dina_LevyLambert/test.py
@@ -1,13 +1,13 @@
 #!/user/bin/python
 
 import random
-from random import randrange
 
 #period lists
 p6 = []
 p7 = []
 
 #reformat line and divide the list of names into their periods
+
 for line in open("ml7-student-names", "r").readlines():
     newLine = line.strip()
     goodLine = newLine.split(',')
@@ -22,14 +22,18 @@ random.shuffle(p6)
 random.shuffle(p7)
 
 
-
-#makes lists for the periods and corresponding headers for them
+#makes lists for the periods and counter
 listPeriods = [p6, p7]
-headers = ["Period 6 groups", "Period 7 groups"]
+counter = 1
 
 #distributes into groups
 for name in listPeriods:
-    print "\n", headers.pop(0)
+    print "\n"
+    if counter == 1 :
+        print "PERIOD 6 GROUPS"
+        counter+= 1
+    else :
+        print "PERIOD 7 GROUPS"
     for num in range(1, len(name) / 4 + 1): #iterate as many times as there are groups
         print "\n\tGROUP " + str(num) + ":"  #print group number
         for num in range(0, 4):
