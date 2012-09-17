a7ef67630a7caa7bd94db53f3afa1914211653ef
Honz <honwei95@gmail.com>
2012-09-16 20:49:59 -0400

    updated program to output into plaintext file
    guis you should rully try dis out

--- HonWeiKhor/ClassRandomizer.py
+++ HonWeiKhor/ClassRandomizer.py
@@ -1,8 +1,10 @@
 import random
 f = open('../ml7-student-names')
+#global list vars for class list
 students_6 = []
 students_7 = []
 
+#segregates student data lists [last, first] into appropriate class lists
 def order(a):
     s = a[0:]
     lname = s[0:s.find(',')]
@@ -15,14 +17,15 @@ def order(a):
     else:
         students_7.append([lname, fname])
 
+#takes groups of 4 students until < 4 students unchosen
 def dosplittythings(s):
     a = s[0:]
     returnval = []
     while (len(a) > 4):
         c = []
         b = random.sample(range(len(a)),4)
-        b.sort()
-        b.reverse()
+        b.sort()#sort so i can reverse
+        b.reverse()#reverse because I want to take out largest element first, array list remove logic
         for i in b:
             c.append(a.pop(i))
         returnval.append(c)
@@ -30,11 +33,18 @@ def dosplittythings(s):
         returnval.append(a)
     return returnval
 
+#makes output file categorizing groups into period number, group number, and group members
+def makeoutputfile(b):
+    g = open("./ml7-student-groups","w")
+    for a in range(0,len(b)):
+        g.write("PERIOD "+str(a+6)+":\n")
+        for i in range(0, len(b[a])):
+            g.write("  Group "+str(i+1)+":\n")
+            for j in range(0, len(b[a][i])):
+                g.write("    "+b[a][i][j][1]+" "+b[a][i][j][0]+"\n")
+
 for line in f:
-#    print(line)
     order(line)
 groups_6 = dosplittythings(students_6)
 groups_7 = dosplittythings(students_7)
-print groups_6
-print groups_7
-raw_input("Press the Enter key to exit.")
+makeoutputfile([groups_6, groups_7])
