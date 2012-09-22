7e0a9c7c17ca123fe5f28964cf6cf7b99d961767
Zachary Zee <Oneman2feet@gmail.com>
2012-09-22 14:38:39 -0400

    test.py now uses a template and provides info to that template

--- 7UP/test.py
+++ 7UP/test.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    info="This text is brought to you from the python file."
+    return render_template('index.html',info=info)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
