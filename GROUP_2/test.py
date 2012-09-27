598336d0625b675ead3e25e2f284ac8a731d537d
David Kurkovskiy <brainmonkey95@gmail.com>
2012-09-27 00:31:27 -0400

    Played around with request

--- GROUP_2/test.py
+++ GROUP_2/test.py
@@ -1,10 +1,22 @@
-from flask import Flask, url_for
+from flask import Flask
+from flask import  url_for, redirect, flask, request
 from flask import render_template
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def home():
-	return "This is David's really cool test page. It's in the works."
+	if request.method == "GET":
+		return render_template("form.html")
+	else:
+		button = request.form['button']
+		name = request.form['username']
+		assert name != ""
+		flash("Name: %s" % (name))
+		return redirect(url_for('home/' + name)
+	#return "This is David's really cool test page. It's in the works."
+	#return url_for('hello')
+	#return redirect(url_for('hello'))
 
 @app.route("/hello")
 @app.route("/hello/<name>")
