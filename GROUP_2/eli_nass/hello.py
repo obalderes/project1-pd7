499bbdfdaf23b0fd701e7718327c47549379ad4f
Eli <eli@eli-VirtualBox.(none)>
2012-09-27 07:52:25 -0400

    form submission

--- GROUP_2/eli_nass/hello.py
+++ GROUP_2/eli_nass/hello.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, url_for, redirect, flash
 
 app = Flask(__name__)
 
@@ -9,8 +9,19 @@ def home():
     
     if request.method == "GET":
         return render_template("t1.html")
+    
     else:
-        return render_template("t2.html")
+        username = request.form['username']
+        button = request.form['button']
+        assert username != ""
+        return redirect(url_for('login',name = username))
+
+@app.route("/login")
+@app.route("/login/<name>")
+def login(name = None):
+    return render_template("t2.html", name = name)
+
+        
 
 
 if __name__ == '__main__':
