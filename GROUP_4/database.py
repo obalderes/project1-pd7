282a167fe4addaac6f4eded2dbddb5810795f5fa
Daryl Sew <darylsew@gmail.com>
2012-09-30 21:40:30 -0700

    fixed tpyo

--- GROUP_4/database.py
+++ GROUP_4/database.py
@@ -6,7 +6,7 @@ import shelve
 
 def fixNames(data):
     """
-    Changes the datafile recieved into a string of first,last,email
+    Changes the datafile received into a string of first,last,email
     Returns the list of names in the format [first,last,email first,last,email...], etc.
     """
     pass
