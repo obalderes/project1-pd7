8749b8e7f7b125469fd8edd7afc92947dd91bb2b
Shan Shi <sshi@thonkpod.(none)>
2012-09-30 22:34:43 -0400

    code is coming together

--- GROUP_6/backend/storage.py
+++ GROUP_6/backend/storage.py
@@ -1,2 +1,9 @@
 import shelve
-s = shelve.open(info.dat)
+s = shelve.open('info.dat')
+s['id'] = []
+s['responses'] = []
+temp = s['id']
+for x in range(0,10):
+    temp.append('john@doe.com')
+s['id'] = temp
+print s
