5338cacdfc3fa7218b0e67e950c484cee9f6acc6
Mengdi Lin <mengdilin95@gmail.com>
2012-10-01 22:19:09 -0400

    revised p1

--- GROUP_5/project1/utils.py
+++ GROUP_5/project1/utils.py
@@ -3,11 +3,8 @@ emails = shelve.open("emails") #key: str(num 0-15) info: emails in lists
 
 def prepro_p1():
     f=open("p1.txt",'r')
-    nameList=[]
     emailList=[]
     key=""
-    comma=0
-    comma2=0
     for line in f.readlines():
         line= line.strip()
         if line in ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15']:
