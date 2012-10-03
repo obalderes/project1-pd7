79aa3c7e5d2c94bbca04270badb6a5a2d2271e54
Leopold Spohngellert <leopoldsg94@gmail.com>
2012-10-03 00:05:12 -0400

    hey

--- GROUP_3/Project_1_Code/app.py
+++ GROUP_3/Project_1_Code/app.py
@@ -1,1 +1,33 @@
-FLAKDJFOIASUD
+from flask import Flask
+from flask import request
+from flask import render_template
+import utils
+from flask import url_for,redirect,flash
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods = ['GET', 'POST'])
+def home():
+    if request.method == "GET":
+        return render_template("homepage.html")
+    else:
+        button=request.homepage['button']
+        return redirect(url_for('login'))
+    name = request.homepage['email']
+    assert name != ""
+    utils.save_email(name)
+    #flash?
+
+
+@app.route("/login")
+def login():
+    userInfo = utils.get_User()
+#should use email from login on homepage to fill in multi dimensional array
+#with user info
+     return render_template("loginPage.html")
+info about ratings and buttons that lead to new pages to 
+button = request.loginPage['button']
+
+
