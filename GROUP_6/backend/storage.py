2f84f639ec26d957ff283ede7955ee24f00b7ff2
Shan Shi <sshi@thonkpod.(none)>
2012-10-04 13:13:21 -0400

    periods

--- GROUP_6/backend/storage.py
+++ GROUP_6/backend/storage.py
@@ -6,13 +6,17 @@ if not os.path.isfile('info.dat'):
     firre = open('students.txt')
     s['names'] = []
     s['ID'] = []
+    s['period'] = []
     s['responses'] = []
     names = []
     ID = []
+    period = []
     for line in firre.readlines():
         currentstudent = line.split(',')
         ID.append(currentstudent[0])
         names.append(currentstudent[2] + " " + currentstudent[1])
+        period.append(currentstudent[4])
     s['ID'] = ID
     s['names'] = names
+    s['period'] = period
     print s
