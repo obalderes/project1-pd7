0c722b7b85cee1753a9f81952750795e37549182
Jack Stevenson <jskestrel@gmail.com>
2012-10-04 13:14:17 -0400

    server update

--- GROUP_0/server.py
+++ GROUP_0/server.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 app.secret_key = "secret"
 
 @app.route("/", methods = ['GET', 'POST'])
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        username = request.form["username"]
+        #WHAT IS THE NAME OF THE BELOW METHOD
+        if !(database.isUsername(username)):
+            flash("%s is not an authorized email. \nplease enter a valid username."%(username))
+            return redirect(url_for("home"))
+        button = request.form["button"]
+        if button == "
