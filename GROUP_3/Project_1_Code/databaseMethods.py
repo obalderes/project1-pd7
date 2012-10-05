994740ac034aaa9bb0072e78ef3dbb3f15c9f7be
Denis Plotkin <denis.plotkin@gmail.com>
2012-10-04 23:38:03 -0400

    added more db functions

--- GROUP_3/Project_1_Code/databaseMethods.py
+++ GROUP_3/Project_1_Code/databaseMethods.py
@@ -16,7 +16,7 @@ def retrieveStudentInfo(email):
 
 #### groups.db ####
 
-def retrieveGroupMembers(groupNumber): 
+def membersInGroup(groupNumber):
     groupDatabase = shelve.open("groups.db")
     groupMembers = groupDatabase[groupNumber]
     if groupMembers eq []:
@@ -24,6 +24,16 @@ def retrieveGroupMembers(groupNumber):
     else:
         return groupMembers
     groupDatabase.close()
+
+def getGroupNumber(email):
+    studentInfo = retrieveStudentInfo(email)
+    groupNumber = studentInfo[6]
+    return groupNumber
+
+def retrieveGroupMembers(email):
+    groupNumber = getGroupNumber(email)
+    groupMembers = membersInGroup(groupNumber)
+    return groupMembers
 
 
 
@@ -38,10 +48,29 @@ def retrieveGrades(email):
         return grades
     gradesDatabase.close()
 
-def setGrades(grades):
+def setGrades(grades,email):
     gradesDatabase = shelve.open("grades.db")
-    ###more goes here ###
+    gradesList = gradesDatabase[email]
+    question = 0
+    for question in gradesList:
+        question.append[grades[question]]
+        question++
+    
+    gradesDatabase.close()
+
 
 
 #### ratedBy.db ####
    
+def addRatedBy(targetEmail,graderEmail):
+    ratedByDatabase = shelve.open("ratedBy.db")
+    ratedByDatabase[targetEmail].append(graderEmail)
+    ratedByDatabase.close()
+
+def hasBeenRatedBy(targetEmail,graderEmail):
+    ratedByDatabase = shelve.open("ratedBy.db")
+    ratedBy = ratedByDatabase[targetEmail]
+    if graderEmail in ratedBy:
+        return True
+    else:
+        return False
