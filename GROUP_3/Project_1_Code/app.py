64e5f291fc1ae5c20c81af124335638dcfa9ee3e
leopoldsg <leopoldsg94@gmail.com>
2012-10-05 13:53:22 -0300

    Update GROUP_3/Project_1_Code/app.py

--- GROUP_3/Project_1_Code/app.py
+++ GROUP_3/Project_1_Code/app.py
@@ -13,28 +13,30 @@ def home():
     if request.method == "GET":
         return render_template("homepage.html")
     else:
-        button=request.homepage['button']
-        return redirect(url_for('login'))
-    name = request.homepage['email']
+        button=request.homepage['button'] #login button
+        return redirect(url_for('login')) #needs to do authentication before directing to login. could be ID
+    email = request.homepage['email']
     assert name != ""
-    utils.save_email(name)
+    databaseMethods.retrieveStudentInfo(email)
+    #groupNumber = databaseMethods.getGroupNumber(email) <-- DENIS MAKE THIS
+    retrieveGroupMembers(groupNumber)
+    #getMyGrades(email) <--DENIS MAKE THIS
+    #userInfo = databaseMethods.get_User() <-- should access first shelve DENIS made.
+   
+   
+   
     #flash?
 
 
 @app.route("/login", methods = ['GET', 'POST']
-#I THINK WE NEED get and post yet again b/c we want to have inputs on this page as well. 
+#I THINK WE NEED get and post yet again b/c we want to have inputs on this page as well.
 def login():
-    userInfo = utils.get_User()
-#should use email from login on homepage to fill in multi dimensional array
-#with user info
+   
      return render_template("loginPage.html")
-#info about ratings and buttons that lead to new pages to 
-button = request.loginPage['button']
+#info about ratings and buttons that lead to new pages to
+button = request.loginPage['button'] #need to set up buttons or links to go into other group members and give ratings
 #not sure what to write for displaying specific information for each user (previous ratings, fellow group members, etc.)
 
 @app.route("/rate")
 def rate():
-    userInfo = utils.get_User()
     return render_template("rate.html")
-
-
