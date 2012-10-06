05d63161cfd08609a38ec9c1b44cfeac0575391e
David Kurkovskiy <brainmonkey95@gmail.com>
2012-10-06 15:58:33 -0400

    Connected some pages

--- GROUP_2/feedbackerator.py
+++ GROUP_2/feedbackerator.py
@@ -13,18 +13,28 @@ def login():
     else:
         button = request.form['button']
         username = request.form['username']
-        #password = request.form['password']
+        password = request.form['password']
         assert username != ""
-       # assert password != ""
+        assert password != ""
         #flash("User " + username)
         return redirect(url_for('user_page', name = username)) #So frustrated. This is an important note to self, David. When you use url_for, the String input is the name of the method of the page, not it's actual URL. Agh!
 
 @app.route("/home")
-@app.route("/home/<name>")
+@app.route("/home/<name>", methods = ['GET', 'POST'])
 def user_page(name=None):
-    return render_template("user_page.html", name=name)
+    if name == None:
+        return redirect(url_for('login'))
+    
+    if request.method == "GET":
+        return render_template("user_page.html", name=name)
+    else:
+        button = request.form['button']
+        if button == 'Rate':
+            return redirect(url_for('rate_page'))
+        elif button == 'View':
+            return redirect(url_for('view_results'))
 
-"""
+
 @app.route("/rate", methods = ['GET', 'POST'])
 def rate_page():
     if request.method == "GET":
@@ -35,8 +45,8 @@ def rate_page():
     name = request.form['student_rated']
     rating = request.form['rating']
     assert name != ""
-"""
-"""def view_results():
+
+def view_results():
     if request.method == "GET":
         return render_template("results.html")
     else:
@@ -45,8 +55,7 @@ def rate_page():
     name=request.form['student_rated']
     rating = request.form['rating']
     assert name != ""
-    flash("Name:%s Rating:%s"%(name,rating))
-   """                     
+    flash("Name:%s Rating:%s"%(name,rating))                       
 
 if __name__ == "__main__":
     app.debug = True
