bdf2fda0e54cdeda614ff195941f78153815916c
Daryl Sew <darylsew@gmail.com>
2012-10-07 00:25:14 -0700

    See Ratings page completed. Well, the bare essentials. I'll do the bare essentials for the Rate page too, and then add in some stat/visualization stuff to the See Ratings page, and then I'll do a bit of debugging/writing code to catch errors/etc before handing everything over to the template people.

--- GROUP_4/app.py
+++ GROUP_4/app.py
@@ -5,9 +5,11 @@ import sys
 import stat
 
 app = Flask(__name__)
+globalemail = ""
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
+    global globalemail
     """
     On the main page a user will type in their email and press the login button.
     This will lead them to a second page (choice.html).
@@ -15,15 +17,16 @@ def home():
     error = ""
     if request.method == "POST":
         if valid_login(request.form['email'], request.form['IDnum']):
-            return log_the_user_in(request.form['email'])
+            globalemail = str(request.form['email'])
+            return log_the_user_in()
         else:
             error = "Invalid username/password"
-            
 
     return render_template("login.html", error=error)
 
 @app.route("/choose/", methods = ["GET", "POST"])
-def log_the_user_in(email):
+def log_the_user_in():
+    global globalemail
     """
     The user arrives at this page if they login successfully.
     Here, they can choose whether they want to a) see ratings or b) rate people.
@@ -37,8 +40,31 @@ def rate_page():
 
 @app.route("/ratings/", methods = ["GET", "POST"])
 def ratings_page():
-    return render_template("ratings.html")
+    global globalemail
+    """
+    I don't really feel like doing the thing Z talked about in class for this so I'll just hardcode stuff. Might do some fancy visualization stuff with d3.js if I have time and feel like it- this is pretty barebones.
+    """
+    f = open("questions.txt", "r").readlines()
+
+    q1ans = ""
+    q2ans = ""
+    q3ans = ""
+    q4ans = ""
+    for rating in database.getRatings(globalemail):
+        q1ans += str(rating[0]) + " "
+        q2ans += str(rating[1]) + " "
+        q3ans += str(rating[2]) + " "
+        q4ans += str(rating[3]) + " "
     
+    return render_template("ratings.html",
+                           q1=f[0],
+                           q1ans=q1ans,
+                           q2=f[1],
+                           q2ans=q2ans,
+                           q3=f[2],
+                           q3ans=q3ans,
+                           q4=f[3],
+                           q4ans=q4ans)
 
 def valid_login(email, IDnum):
     auth = shelve.open("authen")
--- GROUP_4/test.py
+++ GROUP_4/test.py
@@ -2,8 +2,16 @@ import shelve
 import database
 
 
-#auth = shelve.open("authen")
-#print auth["fallenpwr@gmail.com"]
+auth = shelve.open("authen")
+print auth["jpengsmail@gmail.com"]
 
-groups = shelve.open("groups")
-print groups["fallenpwr@gmail.com"]
+#groups = shelve.open("groups")
+#print groups["fallenpwr@gmail.com"]
+
+#print database.getRatings("jpengsmail@gmail.com")
+"""
+f =  open("questions.txt", "r").readlines()
+
+print f[0].strip()
+print f[0]
+"""
