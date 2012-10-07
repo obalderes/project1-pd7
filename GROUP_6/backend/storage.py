361aff1f2918bf719626a4c14fd12d790ba7b192
Shan Shi <sshi@thonkpod.(none)>
2012-10-07 17:46:19 -0400

    add functions

--- GROUP_6/backend/storage.py
+++ GROUP_6/backend/storage.py
@@ -11,12 +11,31 @@ if not os.path.isfile('info.dat'):
     names = []
     ID = []
     period = []
+    group = []
+    responses = []
     for line in firre.readlines():
         currentstudent = line.split(',')
         ID.append(currentstudent[0])
         names.append(currentstudent[2] + " " + currentstudent[1])
         period.append(currentstudent[4])
-        s['ID'] = ID
-        s['names'] = names
-        s['period'] = period
-        print s
+        responses.append([])
+    s['ID'] = ID
+    s['names'] = names
+    s['period'] = period
+    s['group'] = group
+    s['responses'] = responses
+
+def addRating(email, rating):
+    index = s['ID'].index(email)
+    responses = s['responses'] 
+    responses[index].append(rating)
+    s['responses'] = responses
+
+def getInfo(email):
+    index = s['ID'].index(email)
+    ret = []
+    ret.append(s['names'][index])
+    ret.append(s['period'][index])    
+    ret.append(s['group'][index])
+    ret.append(s['responses'][index])
+    return ret
