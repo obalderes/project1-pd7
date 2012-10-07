5b682438670f7352d94575b1b5e73d363254271d
Brian Yan <sorakeybade@gmail.com>
2012-10-07 13:36:44 -0400

    Um...

--- GROUP_6/backend/storage.py
+++ GROUP_6/backend/storage.py
@@ -1,22 +1,23 @@
 import shelve
 import os
 
-if not os.path.isfile('info.dat'):
-    s = shelve.open('info.dat')
-    firre = open('students.txt')
-    s['names'] = []
-    s['ID'] = []
-    s['period'] = []
-    s['responses'] = []
-    names = []
-    ID = []
-    period = []
-    for line in firre.readlines():
-        currentstudent = line.split(',')
-        ID.append(currentstudent[0])
-        names.append(currentstudent[2] + " " + currentstudent[1])
-        period.append(currentstudent[4])
-    s['ID'] = ID
-    s['names'] = names
-    s['period'] = period
-    print s
+def createDatabase():
+    if not os.path.isfile('info.dat'):
+        s = shelve.open('info.dat')
+        firre = open('students.txt')
+        s['names'] = []
+        s['ID'] = []
+        s['period'] = []
+        s['responses'] = []
+        names = []
+        ID = []
+        period = []
+        for line in firre.readlines():
+            currentstudent = line.split(',')
+            ID.append(currentstudent[0])
+            names.append(currentstudent[2] + " " + currentstudent[1])
+            period.append(currentstudent[4])
+            s['ID'] = ID
+            s['names'] = names
+            s['period'] = period
+            print s
