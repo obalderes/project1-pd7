8e309b4777895c81f72fdb66293e1a33c4f8bc46
Shan Shi <sshi@thonkpod.(none)>
2012-10-08 11:48:04 -0400

    should be working

--- GROUP_6/backend/storage.py
+++ GROUP_6/backend/storage.py
@@ -1,30 +1,33 @@
 import shelve
 import os
 
-def init():
-    if not os.path.isfile('info.dat'):
-        s = shelve.open('info.dat')
-        firre = open('students.txt')
-        name = []
-        ID = []
-        period = []
-        group = []
-        responses = []
-        for line in firre.readlines():
-            currentstudent = line.split(',')
-            ID.append(currentstudent[0])
-            name.append(currentstudent[2] + " " + currentstudent[1])
-            period.append(currentstudent[6])
-            group.append(currentstudent[7])
-            responses.append([])
-        s['ID'] = ID
-        s['name'] = name
-        s['period'] = period
-        s['group'] = group
-        s['responses'] = responses
-        print s
-    else:
-        s = shelve.open('info.dat')
+if not os.path.isfile('info.dat'):
+    s = shelve.open('info.dat')
+    firre = open('students.txt')
+    s['ID'] = []
+    s['name'] = []
+    s['period'] = []
+    s['group'] = []
+    s['responses'] = []
+    name = []
+    ID = []
+    period = []
+    group = []
+    responses = []
+    for line in firre.readlines():
+        currentstudent = line.split(',')
+        ID.append(currentstudent[0])
+        name.append(currentstudent[2] + " " + currentstudent[1])
+        period.append(currentstudent[6])
+        group.append(currentstudent[7])
+        responses.append([])
+    s['ID'] = ID
+    s['name'] = name
+    s['period'] = period
+    s['group'] = group
+    s['responses'] = responses
+else:
+    s = shelve.open('info.dat')
 
 def addRating(email, rating):
     ids = s['ID']
@@ -43,7 +46,6 @@ def getInfo(email):
     ret.append(s['responses'][index])
     return ret
 
-init()
 print s
 addRating('a455898334@gmail.com', '5 stars')
 print s
