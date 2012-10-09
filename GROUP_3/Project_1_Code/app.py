f4c5f1983895ce3727d32fd6405f55e0a30c7f9b
Leopold Spohngellert <leopoldsg94@gmail.com>
2012-10-09 19:48:06 -0400

    for peter

--- GROUP_3/Project_1_Code/app.py
+++ GROUP_3/Project_1_Code/app.py
@@ -28,32 +28,27 @@ def login():
 #needs to do authentication before directing to login. could be ID
     email = request.form['email']
     assert email != ""
-    databaseMethods.saveCurrentStudent(email)    
+    databaseMethods.saveCurrentStudent(email) 
+ 
 
 
 @app.route("/home", methods = ['GET', 'POST'])
 def home():
+    grades = retrieveGrades(email)
+    q1 = getGradeList(0, grades)
+    q2 = getGradeList(1, grades)
+    q3 = getGradeList(2, grades)
+    q4 = getGradeList(3, grades)
     if request.method == "GET":
-        return render_template("home.html")
+        return render_template("home.html", 
+                               q1 = q1, 
+                               q2 = q2, 
+                               q3 = q3, 
+                               q4 = q4)
     else:
         return redirect(url_for('rate/'))
 
-    
-    email = databaseMethods.getCurrentStudent()
-
-    #retrieve the info of the student who logged in
-    databaseMethods.retrieveStudentInfo(email)
-    
-    #get the group number of that student
-    groupNumber = databaseMethods.getGroupNumber(email)
-
-    #get the members of that group
-    retrieveGroupMembers(groupNumber)
-
-    #getMyGrades(email)
-    userInfo = databaseMethods.retrieveGrades(email)
-
-
+    grades = databaseMethods.retrieve
     
 #////put back in
 #need to set up buttons or links to go into other group members and give ratings
@@ -61,8 +56,29 @@ def home():
 
 @app.route("/rate/")
 def rate():
+
+    email = databaseMethods.getCurrentStudent()
+
+    #retrieve the info of the student who logged in
+    studentInfo = databaseMethods.retrieveStudentInfo(email)
+    
+    #get the group number of that student
+    groupNumber = databaseMethods.getGroupNumber(email)
+
+    #get the members of that group
+    groupMembers = retrieveGroupMembers(groupNumber)
+
+    #getMyGrades(email)
+    userInfo = databaseMethods.retrieveGrades(email)
     return render_template("rate.html")
+
+
+def getGradeList( i, grades ):
+    return grades[i]
+
 
 if __name__=="__main__":
     app.debug=True # remove this line to turn off debugging
     app.run() 
+
+
