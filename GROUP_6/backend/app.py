995188881da2649df73c95db5836068cf17cbad9
Brian Yan <sorakeybade@gmail.com>
2012-10-09 23:14:07 -0400

    More changes

--- GROUP_6/backend/app.py
+++ GROUP_6/backend/app.py
@@ -35,21 +35,6 @@ def rate():
     else:
         return redirect(url_for(""))
    
-'''
-def getRatings(email):
-    return storage.s['id',email]
-
-def rate(email,r1,r2,r3,comment):
-    storage.s['responses',email,1] = r1
-    storage.s['responses',email,2] = r2
-    storage.s['responses',email,3] = r3
-    storage.s['responses',email,4] = comment
-
-def getInfo(email):
-    namesList =[]
-    namesList[1] = storage.s[email,firstName]
-    namesList.append(storage.s[email,lastName])
-'''
     
 if __name__=="__main__":
     app.debug=True
