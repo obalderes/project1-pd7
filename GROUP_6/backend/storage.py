cb7e400b61edeaa62ae3b144c3cc160eb6369671
Shan Shi <sshi@thonkpod.(none)>
2012-10-09 21:33:46 -0400

    How do I coded

--- GROUP_6/backend/storage.py
+++ GROUP_6/backend/storage.py
@@ -18,7 +18,7 @@ if not os.path.isfile('info.dat'):
         ID.append(currentstudent[0])
         name.append(currentstudent[2] + " " + currentstudent[1])
         period.append(currentstudent[6])
-        group.append(currentstudent[7])
+        group.append(currentstudent[7][1:2])
         rating1.append([])
         rating2.append([])
         rating3.append([])
@@ -29,25 +29,27 @@ if not os.path.isfile('info.dat'):
     s['period'] = period
     s['group'] = group
     s['rating1'] = rating1
-    s['rating2'] = rating1
+    s['rating2'] = rating2
     s['rating3'] = rating3
     s['comment'] = comment
     s['rater'] = rater
 else:
     s = shelve.open('info.dat')
 
-def addRating(ratee, rater, rating1, rating2, rating3, comment):
-    if not rater in s['rater'][s['ID'].index(ratee)]:
+def addRating(ratee, _rater, _rating1, _rating2, _rating3, _comment):
+    if not _rater in s['rater'][s['ID'].index(ratee)]:
         ids = s['ID']
         index = ids.index(ratee)
         rating1 = s['rating1'] 
         rating2 = s['rating2'] 
         rating3 = s['rating3']
+        comment = s['comment']
         rater = s['rater']  
-        rating1[index].append(rating1)
-        rating2[index].append(rating2)
-        rating3[index].append(rating3)
-        rater[index].append(rater)
+        rating1[index].append(_rating1)
+        rating2[index].append(_rating2)
+        rating3[index].append(_rating3)
+        comment[index].append(_comment)
+        rater[index].append(_rater)
         s['rating1'] = rating1
         s['rating2'] = rating2
         s['rating3'] = rating3
@@ -61,10 +63,11 @@ def getInfo(email):
     ret.append(s['name'][index])
     ret.append(s['period'][index])    
     ret.append(s['group'][index])
-    ret.append(s['rating1'])
-    ret.append(s['rating2'])
-    ret.append(s['rating3'])
-    ret.append(s['comment'])
+    ret.append(s['rating1'][index])
+    ret.append(s['rating2'][index])
+    ret.append(s['rating3'][index])
+    ret.append(s['comment'][index])
+    ret.append(s['rater'][index])
     return ret
 
 def returnIDlist():
@@ -72,8 +75,7 @@ def returnIDlist():
 
 if __name__ == "__main__":
     print s
-    addRating('rybicka.zuzanna@gmail.com','a455898334@gmail.com', 5, 6,7,"coolest person on earth")
+    addRating('rybicka.zuzanna@gmail.com','a455898334@gmail.com','5','6','7','coolest person on earth')
+    addRating('sorakeyblade@gmail.com','a455898334@gmail.com','5','6','7','coolest person on earth')
     print s
     print getInfo('a455898334@gmail.com')
-
-
