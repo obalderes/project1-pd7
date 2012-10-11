4e438448434d31940e2ba0bd33db8fdf6dce82fa
jhaggerty95 <jhaggerty95@gmail.com>
2012-10-10 20:43:50 -0400

    added question/groupmember?

--- GROUP_3/Project_1_Code/app.py
+++ GROUP_3/Project_1_Code/app.py
@@ -31,7 +31,6 @@ def login():
         #databaseMethods.saveCurrentStudent(email) 
         return redirect(url_for('home'))
  
-
     
 @app.route("/home", methods = ['GET', 'POST'])
 def home():
@@ -73,6 +72,7 @@ def home():
 @app.route("/rate/")
 def rate():
     global email
+    
     #email = databaseMethods.getCurrentStudent()
     #retrieve the info of the student who logged in
     studentInfo = databaseMethods.retrieveStudentInfo(email)
@@ -84,14 +84,31 @@ def rate():
     groupMembers = retrieveGroupMembers(groupNumber)
 
     #getMyGrades(email)
+    
+    f = open("question.txt", "r").readlines()
+    q1 = f[0]
+    q2 = f[1]
+    q3 = f[2]
+    q4 = f[3]
+    p1 = groupMembers[0]
+    p2 = groupMembers[1]
+    p3 = groupMembers[2]
+    
     userInfo = databaseMethods.retrieveGrades(email)
-    return render_template("rate.html")
+    return render_template("rate.html",
+                           p1=p1,
+                           p2=p2,
+                           p3=p3,
+                           q1=q1,
+                           q2=q2,
+                           q3=q3,
+                           q4=q4,
 
 
 def getGradeList( i, grades ):
     if len(grades) != 0:
         return grades[i]
-
+    
 def getStudentName():
     global email
     #email = databaseMethods.getCurrentStudent()
