63d8c20b74709cf393464b063e5a57114973c2af
Dexter Tong <schoolpurposes@gmail.com>
2012-10-10 21:45:55 -0400

    data.py updated, tester.py is my data.py tester

---
+++ GROUP_1/tester.py
@@ -0,0 +1,8 @@
+from data import *
+
+d = data()
+print d.checkLogin('schoolpurposes@gmail.com','8758')
+print d.getName('Chabma@gmail.com')
+print d.getGroupNo('ScHOolPuRpOSES@gmaiL.cOm')
+print d.getPeriod('wu.allen154@gmail.com')
+print d.getGroup('obalderes@gmail.com')
