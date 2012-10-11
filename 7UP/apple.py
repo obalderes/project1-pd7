dddf90eb11128370ad362a2e86b8b01073919725
Ben Huber <jdh39@John-Hubers-MacBook-Pro-2.local>
2012-10-10 21:17:31 -0400

    message

--- 7UP/apple.py
+++ 7UP/apple.py
@@ -1,60 +1,100 @@
 from flask import Flask, request, render_template
 import datastorage
 import math
-from flask import url_for,redirect,flash
+from flask import session,url_for,redirect,flash
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
 
-#loggedin = True
-#email = 'iouthwaite1@gmail.com'
-
-@app.route("/")
+global questions
+global email
+global loggedin
+@app.route("/",methods=['GET','POST'])
 def home():
-   loggedin = True
-   email = 'iouthwaite1@gmail.com'
-   if loggedin==False:
-        email=request.form["username"]
-        paswrd=request.form["idnum"]
-        if (database.getuser):
-            loggedin = true
-        #return render_template("index.html", loggedin=loggedin)
+   if email in session:
+      return redirect(url_for('rate'))
    else:
-        info = datastorage.getData(email)
-        projects = []
-        count = 1
-        while(count <= len(info)):
-            projects.append(('project' + count)[])
+      return redirect(url_for('login'))
+        
+        
+
+        
+@app.route('/rate', methods=['GET','POST'])
+def rate():
+   if email in session:
+      if request.method=='POST':
+         rater = email
+         rating1 = request.form('question1')
+         rating2 = request.form('question2')
+         rating3 = request.form('question3')
+         ratee = request.form('ratees')
+         datastorage.ratePerson(rater=rater, ratee=ratee,question=questions[1],score=rating1,comments=None)
+         datastorage.ratePerson(rater=rater, ratee=ratee,question=questions[2],score=rating2,comments=None)
+
+         datastorage.ratePerson(rater=rater, ratee=ratee,question=questions[3],score=rating2,comments=None)
+         info = datastorage.getData(email)
+         projects = []
+         count = 1
+         while(count <= len(info)):
+            projects.append(datastorage.getGroupMembers(email,count))
             count = count + 1
 
-        numratings = 0
-        sumratings = 0
-        scores = []
-        for projs in info:
+
+
+
+         numratings = 0
+         sumratings = 0
+         scores = []
+         for projs in info:
             for ratings in info[projs]:
                 numratings = numratings + 1
                 sumratings = sumratings + info[projs][ratings]['score']
                 scores.append(info[projs][ratings]['score'])
-        avgrating = sumratings / numratings
-        ex = 0
-        for score in scores:
+         avgrating = sumratings / numratings
+
+         ex = 0
+         for score in scores:
             ex = ex + (scores[score]-avgrating) * (scores[score]-avgrating)
-        stdev = sqrt(ex/numratings)
+         stdev = sqrt(ex/numratings)
+
+         first = datastorage.getFirst(email)
+         last = datastorage.getLast(email)
+
+         questions=['how smart was your groupmate', 'how much did they help', 'blah,blah, blah']
+
+
+
+         questionavgs=[]
+         for x in range(len(questions)):
+           r=0
+           for i in range(len(info)):
+              r=r+datastorage.getAvgForQuestion(email,i+1,questions[x])
+           questionavgs.append(r/len(info))
+
+
+
+      return render_template('index.html',loggedin=True,projects=projects,questions=questions,\
+avgrating=avgrating,stdex=stdev,questionavgs=questionavgs)
+
+
+
+
         
-        # I need to get first and last names and groupmembers from databases and we need 
-        #need a questions file.
+@app.route('/login', methods=['GET','POST'])
+def login():
+   if request.method == 'POST':
+        email = request.form["username"]
+        paswrd = request.form["idnum"]
+        if datastorage.isUser(email):
+           loggedin = True
+           session[email]=True
+        return redirect(url_for('home'))
+   return render_template('index.html',loggedin=False,projects=None,questions=questions,avgrating=None,stdex=None,questionavgs=None)
 
-
-
-        datastorage.ratePerson(email, emailratee, question, score, comments)
-
-        print avgscore
-        print stdev
-                              
-        
-                
-                
-        
+@app.route('/logout')
+def logout():
+   session.pop(email, None)
+   return redirect(url_for('home'))
 
 
 if __name__=="__main__":
